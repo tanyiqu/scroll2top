f4ff6b62d620bc28cf9cf3ff359dccb2bea29593
Tanyiqu <1953649096@qq.com>
2021-01-07 11:29:32 +0800

    修复多次点击按钮时出现的bug

--- src/scroll2top.js
+++ src/scroll2top.js
@@ -5,16 +5,21 @@
 
 // 滚动计时器
 let scroll2top_timer = null;
-
+let scroll2top_btn = null;
 
 // 滚动到顶部
 function scrollToTop() {
+    console.log('点击');
+    // 取消当前的监听
+    scroll2top_btn.removeEventListener('click', scrollToTop, false);
     let speed = 75; //定义一个速度，即每隔12毫秒走多少px
     scroll2top_timer = setInterval(function () {
         document.documentElement.scrollTop = document.documentElement.scrollTop - speed;
-        //如果scroll的滚动值为0，也就是到达了页面顶部，需要停止定时器
+        // 到达顶部
         if (document.documentElement.scrollTop <= 0) {
-            clearInterval(scroll2top_timer)
+            clearInterval(scroll2top_timer);
+            // 再给按钮加上监听
+            scroll2top_btn.addEventListener('click', scrollToTop, false);
         }
     }, 10);
 }
@@ -22,11 +27,11 @@ function scrollToTop() {
 
 // 挂载到body中
 window.onload = () => {
-    let scroll2top_btn = document.createElement('div');
+    scroll2top_btn = document.createElement('div');
     scroll2top_btn.className = 'scroll2top';
     document.body.appendChild(scroll2top_btn);
-
-    scroll2top_btn.onclick = scrollToTop;
+    // scroll2top_btn.onclick = scrollToTop;
+    scroll2top_btn.addEventListener('click', scrollToTop, false);
 };
 
 
