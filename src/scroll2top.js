4cd757e3c3e890025eb8c763bcffe5afb9666ab3
Tanyiqu <1953649096@qq.com>
2021-01-07 21:23:23 +0800

    1.2 未完成

--- src/scroll2top.js
+++ src/scroll2top.js
@@ -29,10 +29,13 @@ const scroll2top_css_text = `
     }
 `;
 
+
 // 追加样式
+/*
 let scroll2top_style = document.createElement("style");
 scroll2top_style.innerHTML = scroll2top_css_text;
 document.getElementsByTagName('head').item(0).appendChild(scroll2top_style);
+*/
 
 // 滚动计时器
 let scroll2top_timer = null;
@@ -54,7 +57,7 @@ function scrollToTop() {
     }, 10);
 }
 
-
+/*
 // 挂载到body中
 window.onload = () => {
     // div
@@ -66,9 +69,8 @@ window.onload = () => {
 
     // 修改背景色
     let script = document.querySelector('#scroll2top');
-    let background = script.attributes.background.value;
-    if (background) {
-        scroll2top_btn.style.backgroundColor = background;
+    if (script.attributes.background.value) {
+        scroll2top_btn.style.backgroundColor = script.attributes.background.value;
     }
 
     // 监听滚动事件
@@ -82,3 +84,94 @@ window.onload = () => {
         }
     });
 };
+*/
+
+class Scroll2Top {
+    constructor() {
+        this.css_text = `    
+        body {
+            position: relative;
+        }
+    
+        .scroll2top {
+            display: none;
+            position: fixed;
+            width: 50px;
+            height: 50px; 
+            bottom: 30px;
+            right: 10px;
+            background: #2371ef;
+            border-radius: 10px;
+            cursor: pointer;
+        }
+    
+        .scroll2top .scroll2top_img {
+            width: 100%;
+            height: 100%;
+        }`;
+
+        this.scroll2top_icon_html = `
+        <svg t="1609992089351" class="scroll2top_img icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1115" width="144" height="144"><path d="M512 324.288l256 256L708.288 640 512 443.712 315.712 640 256 580.288z" fill="#ffffff" p-id="1116"></path></svg>
+        `;
+
+        this.btn = document.createElement('div');
+
+        console.log(this.btn);
+    }
+
+
+    // 点击事件，滚动至顶部
+    scrollToTop() {
+        console.log(this.btn);
+        // 取消当前的监听
+        this.btn.removeEventListener('click', this, false);
+        let speed = 75; //定义一个速度，即每隔12毫秒走多少px
+        scroll2top_timer = setInterval(function () {
+            document.documentElement.scrollTop = document.documentElement.scrollTop - speed;
+            // 到达顶部
+            if (document.documentElement.scrollTop <= 0) {
+                clearInterval(scroll2top_timer);
+                // 再给按钮加上监听
+                this.btn.addEventListener('click', this.scrollToTop, false);
+            }
+        }, 10);
+    }
+
+    // 创建dom对象
+    create() {
+        // 追加样式
+        let scroll2top_style = document.createElement('style');
+        scroll2top_style.innerHTML = this.css_text;
+        document.getElementsByTagName('head').item(0).appendChild(scroll2top_style);
+        // 创建btn
+        
+        this.btn.className = 'scroll2top';
+        // 添加图标
+        this.btn.innerHTML = this.scroll2top_icon_html;
+
+        document.body.appendChild(this.btn);
+
+        this.btn.addEventListener('click', this.scrollToTop, false);
+    }
+
+    // 挂载到页面上
+    mount() {
+        console.log('挂载');
+        this.create();
+
+
+        // 监听滚动事件
+        window.addEventListener("scroll", () => {
+            //获取scroll的滚动值
+            let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+            if (scrollTop >= 1000) {
+                this.btn.style.display = 'block';
+            } else {
+                this.btn.style.display = 'none';
+            }
+        });
+    }
+
+}
+
+new Scroll2Top().mount();
